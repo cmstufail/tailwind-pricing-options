be1ffdedf3bd01ed8ad726ef13793df664e10bad
cmstufail <cmstufail@gmail.com>
2025-04-13 15:43:09 +0600

    added toggle navbar, menu icon, close icon

--- src/components/NavBar/Link.jsx
+++ src/components/NavBar/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const link = ({route}) => {
     return (
-        <li className='mr-10 bg-amber-200 rounded-lg px-2 py-1'>
+        <li className='mr-10'>
             <a href={route.path}>{route.name}</a>
         </li>
     );
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './link';
+import { Menu, X } from 'lucide-react';
 
 const navigationData =
     [
@@ -31,12 +32,27 @@ const navigationData =
     ]
 
 const NavBar = () => {
-    return (
-        <nav>
+    const [open, setOpen] = useState(false)
 
-            <ul className='flex'>
+    const links = navigationData.map(route => <Link key={route.id} route={route}></Link>)
+    return (
+        <nav className='flex justify-between mx-10'>
+            <span className='flex' onClick={() => setOpen(!open)}>
                 {
-                    navigationData.map(route => <Link key={route.id} route={route}></Link>)
+                    open ?
+                        <X className='md:hidden'></X> :
+                        <Menu className='md:hidden'></Menu>
+                }
+                <ul className='md:hidden'>
+                    {links}
+                </ul>
+
+                <h3 className='ml-4'>My Navbar</h3>
+            </span>
+            <ul className='md:flex hidden'>
+                {
+                    // navigationData.map(route => <Link key={route.id} route={route}></Link>)
+                    links
                 }
             </ul>
             {/* <ul className='flex'>
@@ -53,6 +69,8 @@ const NavBar = () => {
                 <li className='mr-10'><a href="/about">About</a></li>
                 <li className='mr-10'><a href="/blog">Blog</a></li>
             </ul> */}
+
+            <button className='btn'>Sign in</button>
         </nav>
 
         // give me 5 url information, they will have name, path(url path) and id. i want to use it as navigation data in array of objects
