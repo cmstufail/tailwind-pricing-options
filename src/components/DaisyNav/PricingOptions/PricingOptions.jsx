43c8532e1d1f30b625fe87e1db92c31a5b088259
cmstufail <cmstufail@gmail.com>
2025-04-13 16:57:12 +0600

    added pricingoptions component and created pricing data json file and working progress

---
+++ src/components/DaisyNav/PricingOptions/PricingOptions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PricingOptions = () => {
+    return (
+        <div>
+            give me data for pricing options. each price will have information and will have featured. for my gym
+        </div>
+    );
+};
+
+export default PricingOptions;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -36,14 +36,16 @@ const NavBar = () => {
 
     const links = navigationData.map(route => <Link key={route.id} route={route}></Link>)
     return (
-        <nav className='flex justify-between mx-10'>
+        <nav className='flex justify-between mx-10 mt-4'>
             <span className='flex' onClick={() => setOpen(!open)}>
                 {
                     open ?
                         <X className='md:hidden'></X> :
                         <Menu className='md:hidden'></Menu>
                 }
-                <ul className='md:hidden'>
+                <ul className={`md:hidden absolute text-black duration-1000                    
+                        ${open ? 'top-8' : "-top-40"}
+                    bg-amber-200`}>
                     {links}
                 </ul>
 
